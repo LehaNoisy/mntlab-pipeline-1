fdadc260819714993937adc0114d18e13c742aa3
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-30 17:40:22 +0300

    Update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,1 +1,10 @@
-
+def cred = "amVua2luczpqZW5raW5z"
+def File = new File("${WORKSPACE}/pipeline-achernak-${BUILD_NUMBER}.tar.gz")//.getBytes()
+def connection = new URL( "http://EPBYMINW6122.minsk.epam.com:8081/repository/tomcat/appbackup/pipeline-achernak/${BUILD_NUMBER}/pipeline-achernak-${BUILD_NUMBER}.tar.gz").openConnection()
+connection.setRequestMethod("PUT")
+connection.doOutput = true
+connection.setRequestProperty("Authorization" , "Basic ${cred}")
+def writer = new DataOutputStream(connection.outputStream)
+writer.write (File)
+writer.close()
+println connection.responseCode
