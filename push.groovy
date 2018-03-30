62712ec98a563c3b30dcc3571b244633b4015d8d
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-30 18:36:48 +0300

    Update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,7 +1,7 @@
 def BN = args[0]
 def cred = "YWRtaW46YWRtaW4xMjM"
-def File = new File("pipeline-azaitsau-${BUILD_NUMBER}.tar.gz").getBytes()
-def connection = new URL( "http://EPBYMINW7425.minsk.epam.com:8081/repository/Realise/MNT-pipeline/Pip-artifact/${BUILD_NUMBER}/pipeline-azaitsau-${BUILD_NUMBER}.tar.gz").openConnection()
+def File = new File("pipeline-azaitsau-${BN}.tar.gz").getBytes()
+def connection = new URL( "http://EPBYMINW7425.minsk.epam.com:8081/repository/Realise/MNT-pipeline/Pip-artifact/${BN}/pipeline-azaitsau-${BN}.tar.gz").openConnection()
 connection.setRequestMethod("PUT")
 connection.doOutput = true
 connection.setRequestProperty("Authorization" , "Basic ${cred}")
