dd90fc7b5b0ddcde2ad643e3c5f051e5c6068d98
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-30 17:58:33 +0300

    Update pull.groovy

--- pull.groovy
+++ pull.groovy
@@ -1,8 +1,7 @@
-def WS = args[0]
-def BN = args[1]
+def BN = args[0]
 def cred = "amVua2luczpqZW5raW5z"
 def url = new URL( "http://EPBYMINW6122.minsk.epam.com:8081/repository/tomcat/appbackup/pipeline-achernak/${BN}/pipeline-achernak-${BN}.tar.gz").openConnection()
 url.setRequestProperty("Authorization" , "Basic ${cred}")
-def file = new File("${WS}/pipeline-achernak-${BN}.tar.gz")
+def file = new File("pipeline-achernak-${BN}.tar.gz")
 file << url.inputStream
 println url.responseCode
