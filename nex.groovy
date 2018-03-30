98c2dcc725e602b3d7e1dd1878174d9fe88a2d4d
Eugene Hodin <mushtarda@gmail.com>
2018-03-30 17:40:57 +0300

    Update nex.groovy

--- nex.groovy
+++ nex.groovy
@@ -13,9 +13,9 @@ def rev = args[1]
 
 
 if (args[0] == 'pull'){
-    def url ="http://EPBYMINW1766.minsk.epam.com:8081/repository/artifact-repo/Pipeline/EasyHello/${rev}/pipeline-ykhodzin-${rev}.tar.gz"
-    pull_url ="http://nexus/repository/${repo}/${group}/${group}-$app/${rev}/${group}-${app}-${rev}.tar.gz"
-    new File("${app}-${rev}.tar.gz").withOutputStream { out ->
+    def pull_url ="http://EPBYMINW1766.minsk.epam.com:8081/repository/artifact-repo/Pipeline/EasyHello/${rev}/pipeline-ykhodzin-${rev}.tar.gz"
+    //def pull_url ="http://nexus/repository/${repo}/${group}/${group}-$app/${rev}/${group}-${app}-${rev}.tar.gz"
+    new File("pipeline-ykhodzin-${rev}.tar.gz").withOutputStream { out ->
         def url = new URL(pull_url.toString()).openConnection()
         url.setRequestProperty("Authorization", "Basic ${authString}")
         out << url.inputStream
