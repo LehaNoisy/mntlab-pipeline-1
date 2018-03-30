5b77185bab01c3fc727267d92f8e12053dc3afc6
UladzislauValchkou <35953713+UladzislauValchkou@users.noreply.github.com>
2018-03-30 18:05:12 +0300

    Update pull.groovy

--- pull.groovy
+++ pull.groovy
@@ -1,5 +1,6 @@
+def buid = args[0]
 def cred = "YWRtaW46YWRtaW4xMjM="
-def filename = "pipeline-${BUILD_NUMBER}.tar.gz"
+def filename = "pipeline-${BUILD}.tar.gz"
 def pull_url = new URL("http://epbyminw2471.minsk.epam.com:8081/repository/uvalchkou/pipeline/pipeline/${BUILD_NUMBER}/${filename}").openConnection()
 pull_url.setRequestProperty("Authorization", "Basic ${cred}")
 def file = new File("${WORKSPACE}/${filename}")
