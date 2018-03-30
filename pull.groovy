cf46e28204a61efa0f8ddc0ea8b27ffc1478fd98
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-30 17:41:01 +0300

    Update pull.groovy

--- pull.groovy
+++ pull.groovy
@@ -1,1 +1,6 @@
-
+def cred = "amVua2luczpqZW5raW5z"
+def url = new URL( "http://EPBYMINW6122.minsk.epam.com:8081/repository/tomcat/appbackup/${PROJECT_NAME}-${ARTIFACT_SUFFIX}/${BUILD_NUMBER}/${ARTIFACT_NAME}").openConnection()
+url.setRequestProperty("Authorization" , "Basic ${cred}")
+def file = new File("${WORKSPACE}/pipeline-achernak-${BUILD_NUMBER}.tar.gz")
+file << url.inputStream
+println url.responseCode
