9a8de3944e443d64bbc5782da8bb44ead0007923
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-30 18:34:14 +0300

    Update and rename nexus.groovy to push.groovy

---
+++ push.groovy
@@ -0,0 +1,11 @@
+def BN = args[0]
+def cred = "YWRtaW46YWRtaW4xMjM"
+def File = new File("pipeline-azaitsau-${BUILD_NUMBER}.tar.gz").getBytes()
+def connection = new URL( "http://EPBYMINW7425.minsk.epam.com:8081/repository/Realise/MNT-pipeline/Pip-artifact/${BUILD_NUMBER}/pipeline-azaitsau-${BUILD_NUMBER}.tar.gz").openConnection()
+connection.setRequestMethod("PUT")
+connection.doOutput = true
+connection.setRequestProperty("Authorization" , "Basic ${cred}")
+def writer = new DataOutputStream(connection.outputStream)
+writer.write (File)
+writer.close()
+println connection.responseCode
