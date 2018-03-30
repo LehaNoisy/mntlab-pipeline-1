233d9f812ae8bc397db529abf9414eeb1f4d0440
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-30 18:33:38 +0300

    Update pull.groovy

--- pull.groovy
+++ pull.groovy
@@ -1,6 +1,6 @@
 def BN = args[0]
 def cred = "YWRtaW46YWRtaW4xMjM"
-def url = new URL( "http://EPBYMINW6122.minsk.epam.com:8081/repository/Realise/MNT-pipeline/Pip-artifact/${BUILD_NUMBER}/pipeline-azaitsau-${BUILD_NUMBER}.tar.gz").openConnection()
+def url = new URL( "http://EPBYMINW7425.minsk.epam.com:8081/repository/Realise/MNT-pipeline/Pip-artifact/${BUILD_NUMBER}/pipeline-azaitsau-${BUILD_NUMBER}.tar.gz").openConnection()
 url.setRequestProperty("Authorization" , "Basic ${cred}")
 def file = new File("pipeline-azaitsau-${BUILD_NUMBER}.tar.gz")
 file << url.inputStream
