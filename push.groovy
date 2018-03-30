59bd7a679e8465269b1bb815d4e035496af6c0ee
AlexandrSher <30312859+AlexandrSher@users.noreply.github.com>
2018-03-30 17:58:55 +0300

    Update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,5 +1,4 @@
-def WS = args[0]
-def BN = args[1]
+def BN = args[0]
 def cred = "amVua2luczpqZW5raW5z"
 def url = new URL( "http://EPBYMINW6122.minsk.epam.com:8081/repository/tomcat/appbackup/pipeline-achernak/${BN}/pipeline-achernak-${BN}.tar.gz").openConnection()
 connection.setRequestMethod("PUT")
