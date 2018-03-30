de8310ec1960ea82afa87ec472320614011ea875
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-30 18:27:19 +0300

    Update slave.groovy

--- slave.groovy
+++ slave.groovy
@@ -4,8 +4,8 @@ import static groovyx.net.http.ContentType.*
 def version = args[1]
 def authString = "YWRtaW46YWRtaW4xMjM="
 
-if (args[0] == 'pull'){
-    def pull_url ="http://10.6.205.119:8081/repository/test/repository/PROD/REL/PIPELINE/48/PIPELINE-48-APP.tar.gz"
+if (args[0] == 'download'){
+    def pull_url ="http://10.6.205.119:8081/repository/test/repository/PROD/REL/PIPELINE/${version}/PIPELINE-${version}-APP.tar.gz"
       new File("PIPELINE-48-APP.tar.gz").withOutputStream { out ->
           def url = new URL(pull_url.toString()).openConnection()
           url.setRequestProperty("Authorization", "Basic ${authString}")
