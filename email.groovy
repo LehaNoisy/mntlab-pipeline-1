483128805683fc0687b64de6e4aff1a2320e138f
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-31 17:33:17 +0300

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -1,3 +1,6 @@
+package org.mycompany
+import org.springframework.mail.*
+
 def status = args[1]
 def job_name = args[2]
 def build_number = args[3]
@@ -8,5 +11,12 @@ if (args[0] == 'email'){
     def subject = status + " " + job_name + " " + build_number
     def details =  job_name + "STARTED " + "# " + build_number + "on: " + slave_name + " " + failed_report
 
-    
+    def MailSender mailSender
+    def SimpleMailMessage templateMessage
+    def sendEmail(internalBuild) {
+        def msg = new SimpleMailMessage(templateMessage)
+        msg.text = 'Successful build for ' + new Date()
+        msg.from = 'yomivaf@uemail99.com'
+        mailSender.send(msg)
+    }    
 }
