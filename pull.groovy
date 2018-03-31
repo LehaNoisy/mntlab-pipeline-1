66cc8c3415461ac68c915e6d5f49a9db9592384c
pavelkislouski <36988338+pavelkislouski@users.noreply.github.com>
2018-03-31 16:25:48 +0300

    Update pull.groovy

--- pull.groovy
+++ pull.groovy
@@ -6,5 +6,5 @@ def CONVERTED_CREDS = "${authString}".getBytes().encodeBase64().toString()
         def url ="http://EPBYMINW7296.minsk.epam.com:8081/repository/task11/pipeline/grarc/" + BUILD_NUMBER + "/pipeline-pkislouski-" + BUILD_NUMBER + ".tar.gz"
         def file = new File(WORKSPACE + "/nexus.tar.gz")
         def down = new URL(url).openConnection()
-        down.setRequestProperty("Authorization", "Basic" + CONVERTED_CREDS)
+        down.setRequestProperty("Authorization", "Basic " + CONVERTED_CREDS)
 file << down.inputStream
