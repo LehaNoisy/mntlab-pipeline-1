dfe4a8baafbeb3537cc6056cb8d59c7cf353afb2
NikB97 <35953195+NikB97@users.noreply.github.com>
2018-03-31 12:10:53 +0300

    Update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,5 +1,5 @@
 def BUILD_NUMBER = args[0]
-def cred = "YWRtaW46YWRtaW4xMjM"
+def cred = "YWRtaW46YWRtaW4xMjM="
 def File = new File("pipeline-nbuzin-${BUILD_NUMBER}.tar.gz").getBytes()
 def connection = new URL( "http://EPBYMINW2629.minsk.epam.com:8081/repository/NEXUS-JENKINS/group_pipeline/archive/${BUILD_NUMBER}/pipeline-nbuzin-${BUILD_NUMBER}.tar.gz").openConnection()
 connection.setRequestMethod("PUT")
