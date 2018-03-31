bb2a14aad9718907417d0a820bc8c906cb7c456f
AnnaKo <annakowork@gmail.com>
2018-03-31 16:12:13 +0300

    path edited

--- pullme.groovy
+++ pullme.groovy
@@ -4,9 +4,10 @@ def NEXUS_URL = 'http://EPBYMINW7423.minsk.epam.com:8081'
 def REPO_NAME = 'AKO-maven2-hosted-repo'
 def GROUP = 'PipelineGroup'
 def TYPE = 'tar.gz'
-def ARTEFACT = "AKOart-pipeline-${NUMBER}.${TYPE}"
-def opened = new URL("${NEXUS_URL}/repository/${REPO_NAME}/pipeline/${GROUP}/${NUMBER}/${ARTEFACT}").openConnection()
+def ARTIFACT = 'AKOart-pipeline'
+def ARTFILE = "AKOart-pipeline-${NUMBER}.${TYPE}"
+def opened = new URL("${NEXUS_URL}/repository/${REPO_NAME}/pipeline/${GROUP}/${ARTIFACT}/${NUMBER}/${ARTFILE}").openConnection()
 opened.setRequestProperty("Authorization", "Basic ${AuthEncoded}")
-def out = new File("${ARTEFACT}")
+def out = new File("${ARTFILE}")
 out << opened.inputStream
 println opened.responseCode
