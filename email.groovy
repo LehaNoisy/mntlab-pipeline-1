4c07a3ba15f10777b4845ffb581a72a8dd613ec2
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-31 17:29:41 +0300

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -1,13 +1,3 @@
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
-import static groovyx.net.http.ContentType.*  
-import jenkins.*
-import jenkins.model.*
-import hudson.*
-import hudson.model.*
-import javax.mail.internet.*
-import javax.mail.*
-import javax.activation.*
-
 def status = args[1]
 def job_name = args[2]
 def build_number = args[3]
@@ -17,24 +7,6 @@ def failed_report = args[5]
 if (args[0] == 'email'){
     def subject = status + " " + job_name + " " + build_number
     def details =  job_name + "STARTED " + "# " + build_number + "on: " + slave_name + " " + failed_report
-    //       
-    /*
-    emailext (
-        to: 'yomivaf@uemail99.com',
-        subject: subject,
-        body: details,
-        attachLog: true
-    )
-    */
-    stage('Send email') {
-    def mailRecipients = "yomivaf@uemail99.com"
-    def jobName = currentBuild.fullDisplayName
 
-    emailext body: '''${SCRIPT, template="groovy-html.template"}''',
-        mimeType: 'text/html',
-        subject: "[Jenkins] ${jobName}",
-        to: "${mailRecipients}",
-        replyTo: "${mailRecipients}",
-        recipientProviders: [[$class: 'CulpritsRecipientProvider']]
-}
+    
 }
