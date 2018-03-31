9452325b3315acf12a48067cd02d4dbbad81a5c4
aliaksandrzaitsau <35954552+aliaksandrzaitsau@users.noreply.github.com>
2018-03-31 14:40:29 +0300

    Update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,11 +1,7 @@
-import jenkins.model.*
-jenkins = Jenkins.instance
-
-def student = "azaitsau"
 def BUILD_NUMBER = args[0]
 def cred = "YWRtaW46YWRtaW4xMjM="
-def File = new File("pipeline-${student}-${BUILD_NUMBER}.tar.gz").getBytes()
-def url = new URL( "http://EPBYMINW7425.minsk.epam.com:8081/repository/ForPipekine/MNT-pipeline/Pip-artifact/${BUILD_NUMBER}/pipeline-${student}-${BUILD_NUMBER}.tar.gz").openConnection()
+def File = new File("pipeline-azaitsau-${BUILD_NUMBER}.tar.gz").getBytes()
+def url = new URL( "http://EPBYMINW7425.minsk.epam.com:8081/repository/ForPipekine/MNT-pipeline/Pip-artifact/${BUILD_NUMBER}/pipeline-azaitsau-${BUILD_NUMBER}.tar.gz").openConnection()
 connection.setRequestMethod("PUT")
 connection.doOutput = true
 connection.setRequestProperty("Authorization" , "Basic ${cred}")
