575a9fdd65505af4fa86128d631e1f6d44f2195b
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-31 17:48:40 +0300

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -8,24 +8,20 @@ def slave_name = args[4]
 def failed_report = args[5]
 
 if (args[0] == 'email'){
-    //def subject = status + " " + job_name + " " + build_number
-    //def details =  job_name + "STARTED " + "# " + build_number + "on: " + slave_name + " " + failed_report
-    
     email(status, job_name, build_number, slave_name, failed_report)
 }
 
 def email(status, job_name, build_number, slave_name, failed_report){
-    //def log = currentBuild.rawBuild.getLog(20).join('\n\t\t')
-    //def subject = "${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-    //def consURL = new URL("${env.BUILD_URL}consoleText")
-    
+    def subject = status + " " + job_name + " " + build_number
+    def details =  job_name + "STARTED " + "# " + build_number + "on: " + slave_name + " " + failed_report
     def details = """
         STARTED: Job ${job_name} [${build_number}]
         Runned on slave: ${slave_name}
         """
     emailext (
-        to: 'yomivaf@uemail99.com',
-        subject: "123",
-        body: "123"
+        to: 'skyzzz@mail.ru',
+        subject: subject,
+        body: details,
+        attachLog: false
     )    
 }
