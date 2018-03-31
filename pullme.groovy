a0bfc2b44b68da6c0add22ab4c93945b8d994c04
AnnaKo <annakowork@gmail.com>
2018-03-31 15:50:32 +0300

    up the creds

--- pullme.groovy
+++ pullme.groovy
@@ -1,4 +1,5 @@
 def GROUP_NAME = 'mbean-groovy'
+def AuthEncoded = "YWtvOmFrbw=="
 def NEXUS_URL = 'http://EPBYMINW7423.minsk.epam.com:8081'
 def REPO_NAME = 'AKO-maven2-hosted-repo'
 def GROUP = 'PipelineGroup'
@@ -7,7 +8,6 @@ def TYPE = 'tar.gz'
 def artfile = "http://EPBYMINW7423.minsk.epam.com:8081/repository/AKO-maven2-hosted-repo/PipelineGroup/AKOart-pipeline/33/AKOart-pipeline-33.tar.gz"
 
 new File("${artfile}").withOutputStream { out ->
-    def AuthEncoded = "YWtvOmFrbw=="
     def version = artfile.substring(artfile.lastIndexOf("-")+1, artfile.indexOf("."))
     def opened = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTEFACT}/${version}/${ARTEFACT}-${version}.${TYPE}").openConnection()
     opened.setRequestProperty("Authorization", "Basic ${AuthEncoded}");
