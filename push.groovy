8c030a190f57ebdd7e02e5a99853b7cd072c04de
pavelkislouski <36988338+pavelkislouski@users.noreply.github.com>
2018-03-31 15:53:12 +0300

    Update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,7 +1,7 @@
 def BUILD_NUMBER = arg[0]
 def WORKSPACE = arg[1]
 def authString = "cG5leHVzOnBuZXh1cw=="
-        def url ="http://EPBYMINW7296.minsk.epam.com:8081/repository/task11/pipeline/grarc/"+ BUILD_NUMBER "/pipeline-pkislouski-" + BUILD_NUMBER + ".tar.gz"
+        def url ="http://EPBYMINW7296.minsk.epam.com:8081/repository/task11/pipeline/grarc/" + BUILD_NUMBER + "/pipeline-pkislouski-" + BUILD_NUMBER + ".tar.gz"
         def http = new URL(url).openConnection()
         http.doOutput = true
         http.setRequestMethod("PUT")
