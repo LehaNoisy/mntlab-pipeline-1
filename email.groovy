a070cd4f0ee727bf8bb66088bb7b758a4fa8e740
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-31 17:42:46 +0300

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -1,5 +1,5 @@
-package org.mycompany
-import org.springframework.mail.*
+
+
 
 def status = args[1]
 def job_name = args[2]
@@ -8,15 +8,26 @@ def slave_name = args[4]
 def failed_report = args[5]
 
 if (args[0] == 'email'){
-    def subject = status + " " + job_name + " " + build_number
-    def details =  job_name + "STARTED " + "# " + build_number + "on: " + slave_name + " " + failed_report
+    //def subject = status + " " + job_name + " " + build_number
+    //def details =  job_name + "STARTED " + "# " + build_number + "on: " + slave_name + " " + failed_report
+    
+    email(status, job_name, build_number, slave_name, failed_report)
+}
 
-    def MailSender mailSender
-    def SimpleMailMessage templateMessage
-    def sendEmail(internalBuild) {
-        def msg = new SimpleMailMessage(templateMessage)
-        msg.text = 'Successful build for ' + new Date()
-        msg.from = 'yomivaf@uemail99.com'
-        mailSender.send(msg)
-    }    
+def email(status, job_name, build_number, slave_name, failed_report){
+    //def log = currentBuild.rawBuild.getLog(20).join('\n\t\t')
+    //def subject = "${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    //def consURL = new URL("${env.BUILD_URL}consoleText")
+    
+    def details = """
+        STARTED: Job ${job_name} [${build_number}]
+        Runned on slave: ${slave_name}
+        Log:
+        ${log}"""
+    emailext (
+        to: 'yomivaf@uemail99.com',
+        subject: "123",
+        body: "123",
+        attachLog: true
+    )    
 }
