3e841e1a1c433ea73333cc607c57675da58670aa
AnnaKo <annakowork@gmail.com>
2018-03-31 16:07:09 +0300

    up

--- pullme.groovy
+++ pullme.groovy
@@ -1,10 +1,10 @@
+def NUMBER = 33
 def AuthEncoded = "YWtvOmFrbw=="
 def NEXUS_URL = 'http://EPBYMINW7423.minsk.epam.com:8081'
 def REPO_NAME = 'AKO-maven2-hosted-repo'
 def GROUP = 'PipelineGroup'
 def TYPE = 'tar.gz'
 def ARTEFACT = "AKOart-pipeline-${NUMBER}.${TYPE}"
-def NUMBER = 33
 def opened = new URL("${NEXUS_URL}/repository/${REPO_NAME}/pipeline/${GROUP}/${NUMBER}/${ARTEFACT}").openConnection()
 opened.setRequestProperty("Authorization", "Basic ${AuthEncoded}")
 def out = new File("${ARTEFACT}")
