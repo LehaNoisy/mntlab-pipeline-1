e7ff5bbbe14f2c943c62c06880dd382d139374d9
Alexey <shumilovy@mail.ru>
2018-03-31 13:07:19 +0300

    Update push.groovy

--- push.groovy
+++ push.groovy
@@ -1,7 +1,7 @@
 def BUILD_NUMBER = args[0]
-def cred = "6101e355-0a71-4020-9b2e-edb5f6c86445="
+def cred = "YWRtaW46YWRtaW4xMjM="
 def File = new File("pipeline-ashumilau-${BUILD_NUMBER}.tar.gz").getBytes()
-def connection = new URL( "http://EPBYMINW2470.minsk.epam.com:8081/repository/NEXUS-JENKINS/group_pipeline/archive/${BUILD_NUMBER}/pipeline-ashumilau-${BUILD_NUMBER}.tar.gz").openConnection()
+def connection = new URL( "http://EPBYMINW2470.minsk.epam.com:8081/repository/JenkNexus/group_pipeline/archive/${BUILD_NUMBER}/pipeline-ashumilau-${BUILD_NUMBER}.tar.gz").openConnection()
 connection.setRequestMethod("PUT")
 connection.doOutput = true
 connection.setRequestProperty("Authorization" , "Basic ${cred}")
