0220ff1ce7688a0adf2ea7ebd9da55687193d2c0
aliaksandr-lahutsin <35953755+aliaksandr-lahutsin@users.noreply.github.com>
2018-03-31 16:51:09 +0300

    Update email.groovy

--- email.groovy
+++ email.groovy
@@ -2,12 +2,14 @@ def status = args[1]
 def job_name = args[2]
 def build_number = args[3]
 def slave_name = args[4]
+def failed_report = args[5]
 
 if (args[0] == 'email'){
     def log = currentBuild.rawBuild.getLog(20).join('\n\t\t')
     def subject = "${status}: Job '${job_name} [${build_number}]'"
     def details = """STARTED: Job    ${jobname} [${build_number}]
         Runned on slave: ${slave_name}
+        Failed: ${failed_report}
         
         Last 20 lines in log:
         ${log}"""
