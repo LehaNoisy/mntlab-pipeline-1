f4b6dd41cd52fab7f6b0501902cb3396f78797d2
AnnaKo <annakowork@gmail.com>
2018-04-01 16:42:38 +0300

    artname

--- pushme.groovy
+++ pushme.groovy
@@ -5,13 +5,13 @@ def REPO_NAME = 'AKO-maven2-hosted-repo'
 def GROUP = 'PipelineGroup'
 def TYPE = 'tar.gz'
 def ARTIFACT = 'AKOart-pipeline'
-def ARTFILE = "${ARTIFACT}-${BUILD_NUMBER}.${TYPE}"
-def File = new File("${ARTFILE}").getBytes()
-def take = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${BUILD_NUMBER}/${ARTFILE}").openConnection()
+//def ARTFILE = "${ARTIFACT}-${BUILD_NUMBER}.${TYPE}"
+def art = new File("pipeline-hkavaliova-${BUILD_NUMBER}.tar.gz").getBytes()
+def take = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.tar.gz").openConnection()
 take.setRequestMethod("PUT")
 take.doOutput = true
 take.setRequestProperty("Authorization" , "Basic ${AuthEncoded}")
 def upload = new DataOutputStream(connection.outputStream)
-upload.write (File)
+upload.write (art)
 upload.close()
 println take.responseCode
