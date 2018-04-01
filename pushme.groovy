5a7aac0d7b7fb329d5ebb5acf308d55acfb76f9e
AnnaKo <annakowork@gmail.com>
2018-04-01 17:12:16 +0300

    file

--- pushme.groovy
+++ pushme.groovy
@@ -1,18 +1,18 @@
 def BUILD_NUMBER = args[0]
-def AuthEncoded = "YWRtaW46YWRtaW5hZG1pbg=="
+def AuthEncoded = "YWtvOmFrbw=="
 def NEXUS_URL = 'http://EPBYMINW7423.minsk.epam.com:8081'
 def REPO_NAME = 'AKO-maven2-hosted-repo'
-def GROUP = 'PipeGroup'
+def GROUP = 'PipelineGroup'
 def TYPE = 'tar.gz'
-def ARTIFACT = 'AKOpipe'
+def ARTIFACT = 'AKOart-pipeline'
 //def ARTFILE = "${ARTIFACT}-${BUILD_NUMBER}.${TYPE}"
 def art = new File("pipeline-hkavaliova-${BUILD_NUMBER}.tar.gz").getBytes()
-def take = new URL("http://EPBYMINW7423.minsk.epam.com:8081/repository/AKO-maven2-hosted-repo/PipeGroup/AKOpipe/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.tar.gz").openConnection()
-//def take = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.${TYPE}").openConnection()
+//def take = new URL("http://EPBYMINW7423.minsk.epam.com:8081/repository/AKO-maven2-hosted-repo/PipeGroup/AKOpipe/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.tar.gz").openConnection()
+def take = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.${TYPE}").openConnection()
 take.setRequestMethod("PUT")
 take.doOutput = true
 take.setRequestProperty("Authorization" , "Basic ${AuthEncoded}")
 def upload = new DataOutputStream(take.outputStream)
-upload.write (art)
+upload.write (File)
 upload.close()
 println take.responseCode
