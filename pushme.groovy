fe082cf8b846ad685ca4ecbc976f4bccb54bca3b
AnnaKo <annakowork@gmail.com>
2018-04-01 16:15:54 +0300

    changed to BUILD_NUMBER

--- pushme.groovy
+++ pushme.groovy
@@ -1,13 +1,13 @@
-def NUMBER = args[0]
+def BUILD_NUMBER = args[0]
 def AuthEncoded = "YWtvOmFrbw=="
 def NEXUS_URL = 'http://EPBYMINW7423.minsk.epam.com:8081'
 def REPO_NAME = 'AKO-maven2-hosted-repo'
 def GROUP = 'PipelineGroup'
 def TYPE = 'tar.gz'
 def ARTIFACT = 'AKOart-pipeline'
-def ARTFILE = "${ARTIFACT}-${NUMBER}.${TYPE}"
+def ARTFILE = "${ARTIFACT}-${BUILD_NUMBER}.${TYPE}"
 def File = new File("${ARTFILE}").getBytes()
-def take = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${NUMBER}/${ARTFILE}").openConnection()
+def take = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${BUILD_NUMBER}/${ARTFILE}").openConnection()
 take.setRequestMethod("PUT")
 take.doOutput = true
 take.setRequestProperty("Authorization" , "Basic ${AuthEncoded}")
