9edf3ec07ad412ea6d82c9d888e006f7a40d202c
AnnaKo <annakowork@gmail.com>
2018-04-01 17:18:49 +0300

    upd path

--- pullme.groovy
+++ pullme.groovy
@@ -1,4 +1,4 @@
-def NUMBER = args[0]
+def BUILD_NUMBER = args[0]
 //def NUMBER = 47
 def AuthEncoded = "YWtvOmFrbw=="
 def NEXUS_URL = 'http://EPBYMINW7423.minsk.epam.com:8081'
@@ -6,9 +6,9 @@ def REPO_NAME = 'AKO-maven2-hosted-repo'
 def GROUP = 'PipelineGroup'
 def TYPE = 'tar.gz'
 def ARTIFACT = 'AKOart-pipeline'
-def ARTFILE = "AKOart-pipeline-${NUMBER}.${TYPE}"
-def opened = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${NUMBER}/${ARTFILE}").openConnection()
+//def ARTFILE = "AKOart-pipeline-${NUMBER}.${TYPE}"
+def opened = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.${TYPE}").openConnection()
 opened.setRequestProperty("Authorization", "Basic ${AuthEncoded}")
-def out = new File("${ARTFILE}")
+def out = new File("pipeline-hkavaliova-${BUILD_NUMBER}.${TYPE}")
 out << opened.inputStream
 println opened.responseCode
