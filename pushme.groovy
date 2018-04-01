7925e556af7b0ac764d9b05d7f40134ec6efacce
AnnaKo <annakowork@gmail.com>
2018-04-01 17:06:08 +0300

    full URL

--- pushme.groovy
+++ pushme.groovy
@@ -7,7 +7,8 @@ def TYPE = 'tar.gz'
 def ARTIFACT = 'AKOpipe'
 //def ARTFILE = "${ARTIFACT}-${BUILD_NUMBER}.${TYPE}"
 def art = new File("pipeline-hkavaliova-${BUILD_NUMBER}.tar.gz").getBytes()
-def take = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.${TYPE}").openConnection()
+def take = new URL("http://EPBYMINW7423.minsk.epam.com:8081/repository/AKO-maven2-hosted-repo/PipeGroup/AKOpipe/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.tar.gz").openConnection()
+//def take = new URL("${NEXUS_URL}/repository/${REPO_NAME}/${GROUP}/${ARTIFACT}/${BUILD_NUMBER}/pipeline-hkavaliova-${BUILD_NUMBER}.${TYPE}").openConnection()
 take.setRequestMethod("PUT")
 take.doOutput = true
 take.setRequestProperty("Authorization" , "Basic ${AuthEncoded}")
